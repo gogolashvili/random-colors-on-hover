d9664eebe28563db3d8b6ad61ab472d58493a692
gogolashvili <ggogolashvili91@gmail.com>
2023-02-17 04:36:08 +0400

    finish index and style

--- main.js
+++ main.js
@@ -1,30 +1,8 @@
-let charchoDiv = document.querySelector(".charchoDiv");
-let smallDivs;
+let shidadiv = document.querySelector(".shidadiv");
+let cubediv;
 
-for (let i = 0; i < 598; i++) {
-  smallDivs = document.createElement("div");
-  smallDivs.classList.add("smallDivs");
-  charchoDiv.appendChild(smallDivs);
+for (let i = 0; i < 483; i++) {
+  cubediv = document.createElement("div");
+  cubediv.classList.add("cubediv");
+  shidadiv.appendChild(cubediv);
 }
-
-const boxes = document.querySelectorAll(".smallDivs");
-
-boxes.forEach((box) => {
-  box.addEventListener("mousemove", function () {
-    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-    let y = randomColor;
-    box.style.backgroundColor = randomColor;
-    box.style.opacity = "100%";
-    box.style.boxShadow = "0px 0px 30px 4px" + y;
-    box.style.transitionDuration = "0s";
-  });
-});
-
-boxes.forEach((box) => {
-  box.addEventListener("mouseleave", function () {
-    box.style.backgroundColor = "#505050";
-    box.style.transitionDuration = "2s";
-    box.style.boxShadow = "none";
-    box.style.opacity = "25%";
-  });
-});
